9f758ecf464cfd4bf21e4e3775fe1413b9879d97
SaraSoltani98 <SaraSoltani.SaraSoltani@iths.se>
2026-06-02 21:58:28 +0200

    Update login success handler

--- src/main/java/se/iths/sara/secureloginwithauthenticatorapp/config/AppUserLoginSuccessHandler.java
+++ src/main/java/se/iths/sara/secureloginwithauthenticatorapp/config/AppUserLoginSuccessHandler.java
@@ -1,4 +1,46 @@
 package se.iths.sara.secureloginwithauthenticatorapp.config;
 
-public class AppUserLoginSuccessHandler {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import se.iths.sara.secureloginwithauthenticatorapp.model.AppUser;
+import se.iths.sara.secureloginwithauthenticatorapp.service.AppUserService;
+
+import java.io.IOException;
+
+@Component
+public class AppUserLoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final AppUserService appUserService;
+
+    public AppUserLoginSuccessHandler(AppUserService appUserService) {
+        this.appUserService = appUserService;
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication)
+            throws IOException, ServletException {
+
+        String email = authentication.getName();
+
+        AppUser user = appUserService.findByEmail(email).orElseThrow();
+
+        if (user.isTwoFactorEnabled()) {
+
+            HttpSession session = request.getSession();
+            session.setAttribute("email", email);
+
+            response.sendRedirect("/verify-code");
+
+        } else {
+
+            response.sendRedirect("/profile");
+        }
+    }
 }
